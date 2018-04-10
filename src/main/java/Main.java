3b828ed92c77d8b12fc71ea236232c82ba3a806e
Anna-Lena Schwarzkopf <zckrbrt@gmail.com>
2018-04-10 13:39:11 +0200

    Adds support for writing in files and terminates program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
 
@@ -12,12 +14,12 @@ public class Main {
     final static String newLine = System.getProperty("line.separator");
     private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    private static Executor pfandsystem = Executors.newFixedThreadPool(AUTOMATENANZAHL);
+    private static ExecutorService pfandsystem = Executors.newFixedThreadPool(AUTOMATENANZAHL);
 
     public static void main(String[] args) throws InterruptedException {
         Pfandabgabe pfandabgabe = new Pfandabgabe(AUTOMATENANZAHL);
 
-        logger.info("Parallele Systeme - Aufgabe 1: Pfandabgabe" + newLine + "Anna-Lena Schwarzkopf (62265)" + newLine + newLine);
+        logger.info("Parallele Systeme - Aufgabe 2: Pfandabgabe" + newLine + "Anna-Lena Schwarzkopf (62265)" + newLine + newLine);
 
         for (int i = 0; i < 30; i++) {
             Kunde kunde = new Kunde(i + 1);
@@ -26,5 +28,8 @@ public class Main {
             pfandabgabe.automatBenutzen(kunde);
             Thread.sleep(5 * SECONDS);
         }
+
+        pfandsystem.shutdown();
+
     }
 }
--- src/main/java/Pfandabgabe.java
+++ src/main/java/Pfandabgabe.java
@@ -2,7 +2,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.locks.ReentrantLock;
-
 import java.util.concurrent.locks.Condition;
 
 public class Pfandabgabe {

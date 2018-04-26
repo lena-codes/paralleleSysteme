6036180a3275f39eb705354fe175abd14084dc74
Anna-Lena Schwarzkopf <zckrbrt@gmail.com>
2018-04-26 20:13:03 +0200

    Replaces the existing lock unlock procedure with semaphores, therefore the variable freieAutomaten is obsolete

--- src/main/java/Pfandabgabe.java
+++ src/main/java/Pfandabgabe.java
@@ -2,49 +2,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Condition;
 
 public class Pfandabgabe {
 
-    private final ReentrantLock lock = new ReentrantLock();
-    private final Condition condition = lock.newCondition();
-
     private final Semaphore sem;
-
-    private int freieAutomaten;
 
     private static Logger logger = LoggerFactory.getLogger(Pfandabgabe.class);
 
     public Pfandabgabe(int automatenAnzahl) {
-        this.freieAutomaten = automatenAnzahl;
         sem = new Semaphore(automatenAnzahl);
     }
 
     public void automatBenutzen(Kunde kunde) throws InterruptedException {
-        logger.info("Freie Automaten: " + freieAutomaten);
-        try {
-            while (freieAutomaten == 0) {
-                logger.info("Gerade sind keine Automaten frei.");
-                sem.acquire();
-            }
-            freieAutomaten--;
-            kunde.setPfandabgabe(this);
-            logger.info("Es ist ein Automat frei.");
-            logger.info(kunde + " hat " + kunde.getKorbAnzahl() + " Körbe dabei und gibt diese ab.");
-        } finally {
-            sem.release();
-        }
+        logger.info("Freie Automaten: " + sem.availablePermits());
+        sem.acquire();
+        kunde.setPfandabgabe(this);
+        logger.info(kunde + " hat " + kunde.getKorbAnzahl() + " Körbe dabei und gibt diese ab.");
     }
 
     public void automatFreigeben() {
-        try {
-            freieAutomaten++;
-            sem.release();
-        } finally {
-            sem.release();
-        }
+        sem.release();
     }
-
 
 }
